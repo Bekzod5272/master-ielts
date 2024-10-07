13a4112da0e87860eec6a3110d3c05da6fb40a44
unknown <bekzodmirxolov053@gmail.com>
2024-10-07 18:41:13 +0500

    img

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,19 +3,19 @@ export const dataInfoCourseFirstPage = [
         id: 1,
         btnText: 'IELTS STANDART',
         text: 'Elementary darajadan IELTS darajasiga 6-9 oy ichida erishing! Master IELTS bilan har ikki kunda 2 soatlik darslar o‘tkaziladi. Bu faqatgina darslar emas, balki ingliz tili muhitiga haqiqiy sho‘ng‘ishdir.',
-        path: '/public/images/ielts-standard.png',
+        path: '/images/ielts-standard.png',
     },
     {
         id: 2,
         btnText: 'IELTS INTENSIVE',
         text: 'Pre-intermediate darajasida IELTS darajasiga 3-4 oy ichida erishing! Har kuni 2 soatdan yoki 4 soatdan darslar o\'tkaziladi.',
-        path: '/public/images/bk2.png',
+        path: '/images/bk2.png',
     },
     {
         id: 3,
         btnText: 'IELTS Pro',
         text: 'Pre-IELTS darajasidan IELTS darajasiga atigi 2 oyda erishing! Har kuni 4 soatlik intensiv darslar bilan tez va samarali natijalarga erishing',
-        path: '/public/images/ielts-pro.png',
+        path: '/images/ielts-pro.png',
     },
 ]
 export const dataInfoCourseSecondPage = [
@@ -23,13 +23,13 @@ export const dataInfoCourseSecondPage = [
         id: 4,
         btnText: 'CEFR C1',
         text: 'Yuqorida ko\'rsatilgan barcha IELTS ga oid o\'quv darslarimizni xuddi shunday tarzda CEFR uchun ham tayyorlaymiz. Sizga mos keladigan daraja va jadvalni tanlang.',
-        path: '/public/images/cefr1.png',
+        path: '/images/cefr1.png',
     },
     {
         id: 5,
         btnText: 'Business Speaking',
         text: 'Biznes hamkorlar bilan erkin muloqot qilish uchun. Bizning maxsus kurslarimiz yordamida ishdagi muloqot ko\'nikmalaringizni yaxshilang va muvaffaqiyatga erishing.',
-        path: '/public/images/ielts.speaking.png',
+        path: '/images/ielts.speaking.png',
     },
 ]
 export const dataInfoIelts = [
