6ab01b40c7804b73543e86e5871cafa622c155e2
unknown <bekzodmirxolov053@gmail.com>
2024-10-11 10:08:46 +0500

    some changes

--- src/components/UI/AppMap.vue
+++ src/components/UI/AppMap.vue
@@ -1,8 +1,34 @@
+<template>
+  <div class="map"><a
+      href="https://yandex.uz/maps/org/master_ielts/88085157502/?utm_medium=mapframe&utm_source=maps"
+      class="map-title"
+  >Master Ielts</a><a
+      href="https://yandex.uz/maps/10335/tashkent/category/further_education/184106162/?utm_medium=mapframe&utm_source=maps"
+      class="map-description">Дополнительное образование в Ташкенте</a>
+    <iframe src="https://yandex.uz/map-widget/v1/org/master_ielts/88085157502/?ll=69.287408%2C41.355526&z=16.78"
+            width="100%"
+            height="400" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe>
+  </div>
+</template>
 <script setup lang="ts">
 </script>
-<template>
-  <iframe
-      src="https://yandex.uz/map-widget/v1/?ll=69.287875%2C41.355872&z=16&l=map&pt=69.287875,41.355872,pm2rdl"
-      width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"></iframe>
-</template>
+<style lang="scss">
+.map {
+  position: relative;
+  overflow: hidden;
+}
+
+.map-title {
+  color: #eee;
+  font-size: 12px;
+  position: absolute;
+  top: 0;
+}
+
+.map-description {
+  color: #eee;
+  font-size: 12px;
+  position: absolute;
+  top: 14px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import './assets/base.scss'
 import {createApp} from 'vue'
 import App from './App.vue'
 
-import Vue3Toasity, {type ToastContainerOptions} from 'vue3-toastify';
 
 const app = createApp(App);
 
-app.use(Vue3Toasity)
 app.mount('#app')
