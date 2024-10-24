e789413005e6594462cf89359541a30138c35219
unknown <bekzodmirxolov053@gmail.com>
2024-10-24 17:30:27 +0500

    some

--- src/components/UI/AppSwiper.vue
+++ src/components/UI/AppSwiper.vue
@@ -30,8 +30,8 @@ const onSlideChange = (swiper: { activeIndex: number }) => {
           :key="index"
       >
         <div class="course-list__item__content__info">
-          <div class="about-ielts__header" >
-            <h2  class="about-ielts__title">BIZNING<br/><span>KURSLAR</span></h2>
+          <div class="about-ielts__header">
+            <h2 class="about-ielts__title">BIZNING<br/><span>KURSLAR</span></h2>
           </div>
           <h2 class="course-list__item-title">{{ item.btnText }}</h2>
           <p class="course-list__item-description">{{ item.text }}</p>
@@ -42,36 +42,42 @@ const onSlideChange = (swiper: { activeIndex: number }) => {
   </div>
 </template>
 <style scoped lang="scss">
-  .swiper {
-    .about-ielts__title{
-      font-size: 42px;
+.swiper {
+  .about-ielts__title {
+    font-size: 42px;
+  }
+
+  .course-list__item-description {
+    padding: 5px;
+    font-size: 14px;
+    border: 3px solid #714FFB;
+    border-right: none;
+    border-radius: 7px;
+    font-family: "Segoe Print";
+    clip-path: polygon(0% 10%, 100% -4%, 100% 97%, 0 100%);
+    @media (max-width: 991px) {
+      font-size: 12px;
+      clip-path: polygon(0% 10%, 100% -65%, 100% 97%, 0 100%);
     }
-    .course-list__item-description{
-        padding: 5px;
-        font-size: 14px;
-        border: 3px solid #714FFB;
-        border-right: none;
-        border-radius: 7px;
-        font-family: "Segoe Print";
-        clip-path: polygon(0% 10%, 100% -4%, 100% 97%, 0 100%);
-        @media (max-width: 991px) {
-          font-size: 12px;
-          clip-path: polygon(0% 10%, 100% -65%, 100% 97%, 0 100%);
-        }
-        @media (max-width: 768px) {
-          height: 90px;
-          display: flex;
-          align-items: center;
-          padding: 10px;
-          margin: 0 10px;
-          font-family: GothamProMedium;
-          font-size: 12px;
-          border-top: none;
-        }
+    @media (max-width: 768px) {
+      height: 90px;
+      display: flex;
+      align-items: center;
+      padding: 10px;
+      margin: 0 10px;
+      font-family: GothamProMedium;
+      font-size: 12px;
+      border-top: none;
+      clip-path: polygon(0% 0%, 100% -65%, 100% 97%, 0 100%);
     }
-    .about-ielts__title{
-      margin-top: 55px;
-      margin-bottom: 0;
+    @media (max-width: 576px) {
+      margin: 0;
     }
   }
+
+  .about-ielts__title {
+    margin-top: 55px;
+    margin-bottom: 0;
+  }
+}
 </style>
